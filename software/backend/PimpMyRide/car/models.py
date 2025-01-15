ff27aaad6ca41db7f80185ff20374ddfdc873d45
Chorexxs <oswaldofonseca159@gmail.com>
2025-01-15 17:23:39 +0100

    comenzando a trabajar en las luces de los retrovisores

--- software/backend/PimpMyRide/car/models.py
+++ software/backend/PimpMyRide/car/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+
+class Sensor(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    position = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
+
+
+class Light(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    position = models.CharField(max_length=100)
+    is_on = models.BooleanField(default=False)
